0128dd0211d7d3e2c3ec5b8b2fee748f5047c36c
Cyloci <me@cyloci.com>
2022-03-23 21:25:44 -0600

    Make pretty

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      transitionTimingFunction: {
+        bounce: "cubic-bezier(0, 0.2, 0.5, 3)",
+      },
+    },
+  },
+  plugins: [],
+};
